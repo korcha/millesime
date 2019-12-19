dff92e83e9bbcc7b7c38c96932738d8a40e72583
korcha <e.korchagina@partnersmarkets.com>
2019-12-19 16:39:25 +0300

    js/css/img

--- js/self.js
+++ js/self.js
@@ -9,27 +9,57 @@ if (newsCarousel) {
         newsPosition -= newsWidth * newsCount, newsPosition = Math.max(newsPosition, -newsWidth * (newsListElems.length - newsCount)), newsList.style.marginLeft = newsPosition + "px"
     }
 }
+
 var coursesCarousel = document.querySelector(".courses");
 if (coursesCarousel) {
     var coursesWidth = 420, coursesCount = 1, coursesList = coursesCarousel.querySelector(".courses__list"),
-        coursesListElems = coursesCarousel.querySelectorAll(".courses__item"), coursesPosition = 0, sliderName;
+        coursesListElems = coursesCarousel.querySelectorAll(".courses__item"), coursesPosition = 0, sliderName, autoCarousel=true;
 
     var sliderPrevCourses = function () {
-        coursesPosition += coursesWidth * coursesCount, coursesPosition = Math.min(coursesPosition, 0), coursesList.style.marginLeft = coursesPosition + "px",
+        autoCarousel=false;
+        coursesPosition += coursesWidth * coursesCount, coursesPosition = Math.min(coursesPosition, 0), coursesList.style.marginLeft = coursesPosition + "px";
     };
 
     var sliderNextCourses = function () {
-        coursesPosition -= coursesWidth * coursesCount, coursesPosition = Math.max(coursesPosition, -coursesWidth * (coursesListElems.length - coursesCount)), coursesList.style.marginLeft = coursesPosition + "px",
+        autoCarousel=false;
+        coursesPosition -= coursesWidth * coursesCount, coursesPosition = Math.max(coursesPosition, -coursesWidth * (coursesListElems.length - coursesCount)), coursesList.style.marginLeft = coursesPosition + "px";
     }
 
     coursesCarousel.querySelector(".courses__arrow.slider-arrow--prev").onclick = sliderPrevCourses;
     coursesCarousel.querySelector(".courses__arrow.slider-arrow--next").onclick = sliderNextCourses;
 
+
+    var sliderPrevCoursesAuto = function () {
+        if(autoCarousel==true) {
+            coursesPosition += coursesWidth * coursesCount, coursesPosition = Math.min(coursesPosition, 0), coursesList.style.marginLeft = coursesPosition + "px";
+
+            if (coursesPosition == 0) {
+                setInterval(sliderNextCoursesAuto, 5000);
+            }
+        }
+        else{
+            document.querySelector(".courses__timer").classList.remove('loading');
+        }
+    };
+
+    var sliderNextCoursesAuto = function () {
+        if (autoCarousel == true) {
+            if (coursesPosition == -840) {
+                coursesPosition = 420;
+            }
+            coursesPosition -= coursesWidth * coursesCount, coursesPosition = Math.max(coursesPosition, -coursesWidth * (coursesListElems.length - coursesCount)), coursesList.style.marginLeft = coursesPosition + "px";
+        }
+        else{
+            document.querySelector(".courses__timer").classList.remove('loading');
+        }
+    }
+
+
     if (coursesPosition == 0) {
-        sliderName = sliderNextCourses;
+        sliderName = sliderNextCoursesAuto;
     }
     else if (coursesPosition == -840) {
-        sliderName = sliderPrevCourses;
+        sliderName = sliderPrevCoursesAuto;
     }
 
     setInterval(sliderName, 5000);
